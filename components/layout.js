a46283394eadc1049f01cfab06db9f932305b896
Dan <danielpowell12@msn.com>
2021-02-28 23:19:44 -0700

    Tweaks starter styles

--- components/layout.js
+++ components/layout.js
@@ -14,14 +14,20 @@ export default function Layout({ children, home }) {
       <div className={styles.container}>
         <Head>
           <link rel="icon" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${sample(EMOJI_OF_BUILD)}</text></svg>`} />
+          <title>{siteTitle}</title>
           <meta name="description" content={description} />
           <meta property="og:type" content="website" />
           <meta name="og:title" content={siteTitle} />
           <meta name="og:description" content={description} />
         </Head>
+        {!home && (
+          <header>
+            <a href="/">← Back to home</a>
+          </header>
+        )}
         <main>{children}</main>
       </div>
-    <footer className={styles.footer}>Footer content from the 🏔 of Colorado</footer>
+      <footer className={styles.footer}>Footer content from the 🏔 of Colorado</footer>
     </>
   )
 }
