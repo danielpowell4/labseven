bf72c6cea26814d317034e434dd5a26296c11a97
Dan <danielpowell12@msn.com>
2021-09-29 08:01:40 -0600

    Includes all styles for different colors

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -9,21 +9,23 @@ import productsStyles from "./styles/products.module.css";
 
 export async function getStaticPaths() {
   const allProducts = await getAllProducts();
-  const paths = allProducts.map(product => ({ params: { id: product.ID.toString() }}));
+  const paths = allProducts.map((product) => ({
+    params: { id: product.ID.toString() },
+  }));
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const productData = await getProduct(params.id)
+  const productData = await getProduct(params.id);
   return {
     props: {
-      productData
-    }
-  }
+      productData,
+    },
+  };
 }
 
 const Product = ({ productData }) => {
@@ -39,33 +41,56 @@ const Product = ({ productData }) => {
       </div>
       <div>
         <ul>
-          <li><strong>ID</strong> {productData.ID}</li>
-          <li><strong>Name</strong> {productData.Name}</li>
-          <li><strong>Supplier</strong> {productData.Supplier}</li>
+          <li>
+            <strong>ID</strong> {productData.ID}
+          </li>
+          <li>
+            <strong>Name</strong> {productData.Name}
+          </li>
+          <li>
+            <strong>Supplier</strong> {productData.Supplier}
+          </li>
           <li>
             <strong>Manufacturer</strong>
             <span>
-              {productData.Manufacturer}
-              -
-              {productData.ManufacturerSku}
+              {productData.Manufacturer}-{productData.ManufacturerSku}
             </span>
           </li>
           <li>
             <strong>Categories</strong>
             <ul>
-              {productData.Categories.map(cat => <li key={cat.ID}>{cat.Name} ({cat.ID})</li>)}
+              {productData.Categories.map((cat) => (
+                <li key={cat.ID}>
+                  {cat.Name} ({cat.ID})
+                </li>
+              ))}
             </ul>
           </li>
           <li>
             <strong>UnitPrice</strong>
             {productData.UnitPrice}
           </li>
+          <li>
+            <strong>Styles</strong>
+            <ul style={{ display: "flex", flexFlow: "row wrap", gap: "1rem" }}>
+              {productData.Styles.map((style) => (
+                <li key={style.ID}>
+                  <div
+                    style={{
+                      width: 40,
+                      height: 40,
+                      backgroundColor: `#${style.HtmlColor1}`,
+                    }}
+                  />
+                  {style.Name}
+                </li>
+              ))}
+            </ul>
+          </li>
         </ul>
         <hr />
         <details className={productsStyles.details}>
-          <summary>
-            View full productData
-          </summary>
+          <summary>View full productData</summary>
           <pre>{JSON.stringify(productData, null, 2)}</pre>
         </details>
       </div>
