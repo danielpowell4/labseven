7a8ba08a8192389e9a4997971ee0a060567f5084
Dan <danielpowell12@msn.com>
2021-10-24 12:49:21 -0600

    Begins building out search results

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,13 +1,25 @@
 import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
-import Layout from "../../components/layout";
+import { ColorOptions, Layout } from "../../components";
 import { getAllProducts } from "../../lib/products";
 
-import utilStyles from "../../styles/utils.module.css";
 import productsStyles from "./styles/products.module.css";
 
 export async function getStaticProps() {
-  const allProductData = await getAllProducts();
+  const allProducts = await getAllProducts();
+  // Hide products without categories!
+  const allProductData = allProducts.filter((product) => !!product.Categories.length)
+    .sort((a, b) => {
+      const supplierCompare = a.Supplier.localeCompare(b.Supplier);
+      if (supplierCompare !== 0) return supplierCompare;
+
+      const manufacturerCompare = a.Manufacturer.localeCompare(b.Manufacturer);
+      if (manufacturerCompare !== 0) return manufacturerCompare;
+
+      return a.ManufacturerSku.localeCompare(b.ManufacturerSku);
+    });
+
   return {
     props: {
       allProductData,
@@ -15,150 +27,53 @@ export async function getStaticProps() {
   };
 }
 
+const ProductCard = ({ product }) => {
+  const activeStyle = product.Styles[0];
+
+  return <li className={productsStyles.ProductCard}>
+    <Link href={product.defaultHref}>
+      <a>
+        { activeStyle.hasMainImage ? (
+                <Image
+                  src={activeStyle.mainImageUrl}
+                  objectFit="contain"
+                  objectPosition="center"
+                  width={380}
+                  height={410}
+                  alt={`Sample of ${product.Name} in ${activeStyle.Name} style`}
+                />
+              ) : (
+                <p>Missing image!</p>
+              )}
+        <h4>{product.Manufacturer} {product.ManufacturerSku}</h4>
+        <p>{product.Name}</p>
+        <p>{`$${product.UnitPrice.toFixed(2)}`} | {product.Styles.length > 1 ? `${product.Styles.length} Colors` : "1 Color"}</p>
+        {product.Styles.length > 1 && <ColorOptions Styles={product.Styles} activeStyle={activeStyle} />}
+      </a>
+      </Link>
+  </li>
+}
+
 const Products = ({ allProductData }) => {
-  const productsWithCats = allProductData
-    .filter((product) => !!product.Categories.length)
-    .sort((a, b) => {
-      const supplierCompare = a.Supplier.localeCompare(b.Supplier);
-      if (supplierCompare !== 0) return supplierCompare;
-
-      const manufacturerCompare = a.Manufacturer.localeCompare(b.Manufacturer);
-      if (manufacturerCompare !== 0) return manufacturerCompare;
-
-      return a.ManufacturerSku.localeCompare(b.ManufacturerSku);
-    });
-  const productsWithoutCats = allProductData
-    .filter((product) => !product.Categories.length)
-    .sort((a, b) => {
-      const supplierCompare = a.Supplier.localeCompare(b.Supplier);
-      if (supplierCompare !== 0) return supplierCompare;
-
-      const manufacturerCompare = a.Manufacturer.localeCompare(b.Manufacturer);
-      if (manufacturerCompare !== 0) return manufacturerCompare;
-
-      return a.ManufacturerSku.localeCompare(b.ManufacturerSku);
-    });
-
   return (
     <Layout>
       <Head>
         <title>Shop our products</title>
       </Head>
-      <aside>
-        <h3>Apparel</h3>
-      </aside>
-      <main>
-        <h1>Shop our products</h1>
-        <details className={productsStyles.details}>
-          <summary>products WITH categories {productsWithCats.length}</summary>
-          <div>
-            <table className={utilStyles.table}>
-              <thead>
-                <tr>
-                  <th style={{ width: 320 }}>Product Name</th>
-                  <th>Supplier</th>
-                  <th>Manufacturer + Sku</th>
-                  <th>Categories</th>
-                  <th>UnitPrice</th>
-                  <th>Styles</th>
-                </tr>
-              </thead>
-              <tbody>
-                {productsWithCats.map((product) => (
-                  <tr key={product.ID}>
-                    <td>{product.Name}</td>
-                    <td>{product.Supplier}</td>
-                    <td>
-                      {product.Manufacturer}
-                      <br />
-                      {product.ManufacturerSku}
-                    </td>
-                    <td>
-                      <ul>
-                        {product.Categories.map((cat) => (
-                          <li key={cat.ID}>
-                            {cat.Name} ({cat.ID})
-                          </li>
-                        ))}
-                      </ul>
-                    </td>
-                    <td>{product.UnitPrice}</td>
-                    <td>
-                      <ul>
-                        {product.Styles.map((style) => (
-                          <li key={style.id}>
-                            <Link
-                              href={`/products/${product.manufacturerSkuCode}/${style.nameCode}`}
-                            >
-                              <a>{style.Name}</a>
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </details>
-        <details className={productsStyles.details}>
-          <summary>
-            products without categories {productsWithoutCats.length}
-          </summary>
-          <div>
-            <table className={utilStyles.table}>
-              <thead>
-                <tr>
-                  <th style={{ width: 320 }}>Product Name</th>
-                  <th>Supplier</th>
-                  <th>Manufacturer + Sku</th>
-                  <th>Categories</th>
-                  <th>UnitPrice</th>
-                  <th>Styles</th>
-                </tr>
-              </thead>
-              <tbody>
-                {productsWithoutCats.map((product) => (
-                  <tr key={product.ID}>
-
-                    <td>{product.Name}</td>
-                    <td>{product.Supplier}</td>
-                    <td>
-                      {product.Manufacturer}
-                      <br />
-                      {product.ManufacturerSku}
-                    </td>
-                    <td>
-                      Not added in InkSoft
-                    </td>
-                    <td>{product.UnitPrice}</td>
-                    <td>
-                      <ul>
-                        {product.Styles.map((style) => (
-                          <li key={style.id}>
-                            <Link
-                              href={`/products/${product.manufacturerSkuCode}/${style.nameCode}`}
-                            >
-                              <a>{style.Name}</a>
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </details>
-        <details className={productsStyles.details}>
-          <summary>
-            raw products: {JSON.stringify(allProductData.length, null, 2)}
-          </summary>
-          <pre>allProductData: {JSON.stringify(allProductData, null, 2)}</pre>
-        </details>
-      </main>
+      <div className={productsStyles.searchContainer}>
+        <aside className={productsStyles.searchContainer__aside}>
+          <h3>Apparel</h3>
+          <pre>TODO: add filters</pre>
+        </aside>
+        <main className={productsStyles.searchContainer__main}>
+          <h1>Shop our products</h1>
+          <ul className={productsStyles.ProductList}>
+            {allProductData.map((product) => (
+              <ProductCard key={product.ID} product={product} />
+            ))}
+          </ul>
+        </main>
+      </div>
     </Layout>
   );
 };
