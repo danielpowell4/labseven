107ad8e67e8306d33fa91272e9b18eb1001bdba7
Dan <danielpowell12@msn.com>
2022-06-24 08:55:56 -0600

    Drops in logo, flags

--- components/SiteNav.js
+++ components/SiteNav.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Image from "next/image";
 import Link from "next/link";
 import styles from "./SiteNav.module.css";
 
@@ -13,11 +14,13 @@ const LINKS = [
 const SiteNav = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>LOGO</div>
+      <div className={styles.logo}>
+        <img src="/assets/Lab-Seven-Logo.svg" height="auto" width={220} />
+      </div>
       <nav className={styles.nav}>
         <ul>
-          {LINKS.map(({ href, children }) => (
-            <li>
+          {LINKS.map(({ href, children }, linkIndex) => (
+            <li key={linkIndex}>
               <Link href={href}>
                 <a>{children}</a>
               </Link>
@@ -25,7 +28,10 @@ const SiteNav = () => {
           ))}
         </ul>
       </nav>
-      <div className={styles.locales}>FLAGS</div>
+      <div className={styles.locales}>
+        <Image src="/assets/Colorado-Flag.svg" height={20} width={30} />
+        <Image src="/assets/USA-Flag.svg" height={20} width={30} />
+      </div>
     </header>
   );
 };
