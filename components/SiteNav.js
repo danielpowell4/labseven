aa1d6ea8311895aa299935034c305c704c74788e
Dan <danielpowell12@msn.com>
2022-06-24 09:25:14 -0600

    Starts callNow flyout

--- components/SiteNav.js
+++ components/SiteNav.js
@@ -1,15 +1,6 @@
 import * as React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./SiteNav.module.css";
-
-const LINKS = [
-  { href: "", children: "Link 1" },
-  { href: "", children: "Link 2" },
-  { href: "", children: "Link 3" },
-  { href: "", children: "Link 4" },
-  { href: "", children: "Link 5" },
-];
 
 const SiteNav = () => {
   return (
@@ -17,17 +8,34 @@ const SiteNav = () => {
       <div className={styles.logo}>
         <img src="/assets/Lab-Seven-Logo.svg" height="auto" width={220} />
       </div>
-      <nav className={styles.nav}>
-        <ul>
-          {LINKS.map(({ href, children }, linkIndex) => (
-            <li key={linkIndex}>
-              <Link href={href}>
-                <a>{children}</a>
-              </Link>
+      <div className={styles.callNow}>
+        <button className={styles.callNow__anchor}>Call Now</button>
+        <div className={styles.callNow__flyout}>
+          <ul>
+            <li>
+              <span className={styles.callNow__flyout__label}>
+                River Point
+                <sub className={styles.callNow__flyout__label__accent}>HQ</sub>
+              </span>
+              <a
+                href="tel:+13038413389"
+                className={styles.callNow__flyout__number}
+              >
+                {`(303) 841-3389`}
+              </a>
             </li>
-          ))}
-        </ul>
-      </nav>
+            <li>
+              <span className={styles.callNow__flyout__label}>Lakeside</span>
+              <a
+                href="tel:+17207086192"
+                className={styles.callNow__flyout__number}
+              >
+                {`(720) 708-6192`}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className={styles.locales}>
         <Image src="/assets/Colorado-Flag.svg" height={20} width={30} />
         <Image src="/assets/USA-Flag.svg" height={20} width={30} />
