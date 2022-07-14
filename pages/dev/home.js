3f30b746aa31377a05672dec553bf4b55b2ca2f4
Dan <danielpowell12@msn.com>
2022-07-13 23:50:51 -0600

    Drops in links

--- pages/dev/home.js
+++ pages/dev/home.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Head from "next/head";
+import Link from "next/link";
 import { Layout } from "../../components";
 
 import styles from "./Home.module.css";
@@ -8,8 +9,8 @@ const ACTION_VERBS = [
   "Wear",
   "Share",
   "Put on",
-  "Give out",
-  "Put on",
+  "Sell",
+  "Wear",
   "Share",
   "Wear",
 ];
@@ -20,8 +21,8 @@ const HomePage = () => {
       <Head>
         <meta name="robots" content="noindex" />
       </Head>
-      <div>
-        <div>
+      <div className={styles.hero}>
+        <div className={styles.hero__text}>
           <h1>
             <div className={styles.verbsContainer}>
               <div className={styles.verbsContainer__reel}>
@@ -34,7 +35,18 @@ const HomePage = () => {
               <span>your brand with pride</span>
             </div>
           </h1>
-          <h2>Get Quality Custom Apparel Locally and Affordably</h2>
+          <h2>
+            Get Custom Printed Apparel Affordably. Backed by a Local Team You
+            Can Trust.
+          </h2>
+          <div className={styles.linkContainer}>
+            <Link href="/products">
+              <a className={styles.LinkButton}>Browse Catalog</a>
+            </Link>
+            <Link href="/contact">
+              <a className={styles.LinkButtonAlternate}>Contact Printing Pro</a>
+            </Link>
+          </div>
         </div>
       </div>
     </Layout>
