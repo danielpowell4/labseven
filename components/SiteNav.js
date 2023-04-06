37fc49ef04fe11530df3cd0ff31dcdb72d6cfc17
Dan Powell <dan@gobloom.io>
2023-04-05 22:25:52 -0600

    Adds spacer to nav

--- components/SiteNav.js
+++ components/SiteNav.js
@@ -8,49 +8,55 @@ const SiteNav = () => {
 
   return (
     <header className={styles.header}>
-      <a className={styles.logo} href="//labseven.co">
-        <img
-          alt="Lab Seven Screen Printing Co. Logo"
-          src="/assets/Lab-Seven-Logo.svg"
-          height={40.5}
-          width={220}
-        />
-      </a>
-      <div
-        className={`${styles.callNow} ${isOpen ? styles.callNow__isOpen : ""}`}
-        ref={callContainerRef}
-        onMouseLeave={() => setIsOpen(false)}
-      >
-        <button
-          className={styles.callNow__anchor}
-          onMouseEnter={() => setIsOpen(true)}
+      <div className={styles.header__spacer}>
+        <a className={styles.logo} href="//labseven.co">
+          <img
+            alt="Lab Seven Screen Printing Co. Logo"
+            src="/assets/Lab-Seven-Logo.svg"
+            height={40.5}
+            width={220}
+          />
+        </a>
+        <div
+          className={`${styles.callNow} ${
+            isOpen ? styles.callNow__isOpen : ""
+          }`}
+          ref={callContainerRef}
+          onMouseLeave={() => setIsOpen(false)}
         >
-          Call Now
-        </button>
-        <div className={styles.callNow__flyout}>
-          <ul>
-            <li>
-              <span className={styles.callNow__flyout__label}>
-                River Point
-                <sub className={styles.callNow__flyout__label__accent}>HQ</sub>
-              </span>
-              <a
-                href="tel:+13038143389"
-                className={styles.callNow__flyout__number}
-              >
-                {`(303) 814-3389`}
-              </a>
-            </li>
-            <li>
-              <span className={styles.callNow__flyout__label}>Lakeside</span>
-              <a
-                href="tel:+17207086192"
-                className={styles.callNow__flyout__number}
-              >
-                {`(720) 708-6192`}
-              </a>
-            </li>
-          </ul>
+          <button
+            className={styles.callNow__anchor}
+            onMouseEnter={() => setIsOpen(true)}
+          >
+            Call Now
+          </button>
+          <div className={styles.callNow__flyout}>
+            <ul>
+              <li>
+                <span className={styles.callNow__flyout__label}>
+                  River Point
+                  <sub className={styles.callNow__flyout__label__accent}>
+                    HQ
+                  </sub>
+                </span>
+                <a
+                  href="tel:+13038143389"
+                  className={styles.callNow__flyout__number}
+                >
+                  {`(303) 814-3389`}
+                </a>
+              </li>
+              <li>
+                <span className={styles.callNow__flyout__label}>Lakeside</span>
+                <a
+                  href="tel:+17207086192"
+                  className={styles.callNow__flyout__number}
+                >
+                  {`(720) 708-6192`}
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className={styles.locales}>
