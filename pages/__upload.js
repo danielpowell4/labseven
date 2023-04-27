1949af98b0c163dcf00521caa6a0d3e40a9ee377
Dan Powell <dan@gobloom.io>
2023-04-26 22:33:13 -0600

    Refactors upload to live in hook

--- pages/__upload.js
+++ pages/__upload.js
@@ -3,142 +3,19 @@ import Head from "next/head";
 
 import { Button, Layout } from "../components";
 
-import { Dropbox } from "dropbox";
-
-import useSWR from "swr";
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+import useFileUpload from "lib/useFileUpload";
 
 export async function getStaticProps() {
   return { props: {} };
 }
 
-const UPLOAD_FILE_SIZE_LIMIT = 150 * 1024 * 1024;
-
-const MonthNameMap = {
-  0: "January",
-  1: "February",
-  2: "March",
-  3: "April",
-  4: "May",
-  5: "June",
-  6: "July",
-  7: "August",
-  8: "September",
-  9: "October",
-  10: "November",
-  11: "December",
-};
-
-/** from https://github.com/dropbox/dropbox-sdk-js/blob/main/examples/javascript/upload/index.html */
-const uploadFile = async (dropbox, path, file) => {
-  if (file.size < UPLOAD_FILE_SIZE_LIMIT) {
-    // File is smaller than 150 Mb - use filesUpload API
-
-    return dropbox
-      .filesUpload({ path: path + file.name, contents: file })
-      .then(function (response) {
-        console.log("upload success", response);
-      });
-  } else {
-    // File is bigger than 150 Mb - use filesUploadSession* API
-
-    const maxBlob = 8 * 1000 * 1000; // 8Mb - Dropbox JavaScript API suggested max file / chunk size
-    const workItems = [];
-    let offset = 0;
-
-    while (offset < file.size) {
-      var chunkSize = Math.min(maxBlob, file.size - offset);
-      workItems.push(file.slice(offset, offset + chunkSize));
-      offset += chunkSize;
-    }
-
-    const bigFileTask = workItems.reduce((acc, blob, idx, items) => {
-      if (idx == 0) {
-        // Starting multipart upload of file
-        return acc.then(function () {
-          return dropbox
-            .filesUploadSessionStart({ close: false, contents: blob })
-            .then((response) => response.session_id);
-        });
-      } else if (idx < items.length - 1) {
-        // Append part to the upload session
-        return acc.then((sessionId) => {
-          const cursor = { session_id: sessionId, offset: idx * maxBlob };
-          return dropbox
-            .filesUploadSessionAppendV2({
-              cursor: cursor,
-              close: false,
-              contents: blob,
-            })
-            .then(() => sessionId);
-        });
-      } else {
-        // Last chunk of data, close session
-        return acc.then(function (sessionId) {
-          const cursor = {
-            session_id: sessionId,
-            offset: file.size - blob.size,
-          };
-          const commit = {
-            path: path + file.name,
-            mode: "add",
-            autorename: true,
-            mute: false,
-          };
-          return dropbox.filesUploadSessionFinish({
-            cursor: cursor,
-            commit: commit,
-            contents: blob,
-          });
-        });
-      }
-    }, Promise.resolve()); // start w/ promise chain
-
-    return bigFileTask.then(function (bigFileResponse) {
-      console.log("bigFileResponse", bigFileResponse);
-    });
-  }
-};
-
 const Upload = () => {
-  const [data, setData] = React.useState("Idle");
-  const tokenQuery = useSWR("/api/fileUpload/getToken", fetcher);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // stop the page from refreshing
-
-    setData("Upload started!");
-
-    const formEl = e.currentTarget;
-    const companyName = formEl.elements.companyName.value ?? "no-company-name";
-    const fileInput = formEl.elements.fileUpload;
-    const dropbox = new Dropbox({
-      accessToken: tokenQuery.data?.token,
-    });
-
-    const today = new Date();
-    const UPLOAD_PATH = [
-      "",
-      "__test",
-      `${MonthNameMap[today.getMonth()]} ${today.getFullYear()}`,
-      companyName,
-      "",
-    ].join("/");
-
-    try {
-      for (let i = 0; i < fileInput.files.length; i++) {
-        const file = fileInput.files[i];
-        setData(`file #${i + 1} name: ${file.name} size: ${file.size}`);
-        await uploadFile(dropbox, UPLOAD_PATH, file);
-        setData(
-          `Upload complete! Check Dropbox App folder for: ${UPLOAD_PATH}`
-        );
-      }
-    } catch (error) {
-      console.error(error);
-      setData("Upload failed! check console for details");
-    }
-  };
+  const [name, setName] = React.useState("Test Name");
+  const [attachments, setAttachments] = React.useState([]);
+  const addAttachment = React.useCallback((attachment) => {
+    setAttachments((prev) => [...prev, attachment]);
+  });
+  const [data, onInputChange] = useFileUpload(addAttachment);
 
   return (
     <Layout>
@@ -149,30 +26,38 @@ const Upload = () => {
         <h1>Dropbox File Upload</h1>
         <p>This page is intended as a sandbox for testing the Dropbox API.</p>
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(e) => {
+            e.preventDefault();
+            alert("You submitted somehow?!");
+          }}
           style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
         >
           <label htmlFor="companyName">Company Name</label>
-          <input type="text" id="companyName" name="companyName" required />
+          <input
+            type="text"
+            id="companyName"
+            name="companyName"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
 
           <label htmlFor="fileUpload">File Upload</label>
-          <input type="file" id="fileUpload" multiple required />
-
-          <Button type="submit" style={{ maxWidth: 220 }}>
-            Upload File
-          </Button>
+          <input
+            type="file"
+            id="fileUpload"
+            onChange={(e) => onInputChange(name, e)}
+            multiple
+            required
+          />
+          <pre>attachments: {JSON.stringify(attachments, null, 2)}</pre>
         </form>
 
         <hr />
 
         <details open>
-          <summary>Dev Helper</summary>
+          <summary>Upload state</summary>
           <pre>data: {JSON.stringify(data, null, 2)}</pre>
-        </details>
-
-        <details open>
-          <summary>tokenQuery</summary>
-          <pre>{JSON.stringify(tokenQuery)}</pre>
         </details>
       </div>
     </Layout>
