b9677730a0151d7c40fd4965ddd55853bd4c71b2
Dan Powell <dan@gobloom.io>
2023-05-03 19:04:01 -0600

    Drops in skeleton content

--- components/Services/ScreenPrinting.js
+++ components/Services/ScreenPrinting.js
@@ -1,72 +1,93 @@
+import Image from "next/image";
+
 import { Button } from "/components";
+
+import Banner from "public/assets/Services/ScreenPrinting_Banner.jpg";
+
+import HopeHoodies from "public/assets/Services/ScreenPrinting_01_HopeHoodies.jpg";
+import PhillipLindsay from "public/assets/Services/ScreenPrinting_02_PhillipLindsay.jpg";
+import CatDog from "public/assets/Services/ScreenPrinting_03_CatDog.jpg";
+
+import IconMockup from "public/assets/Services/ScreenPrinting_IconMockup.svg";
+
+import styles from "./Services.module.css";
 
 const ScreenPrinting = () => {
   return (
-    <section>
-      <div>
-        <div>Header Image</div>
-        <div>
+    <section id="ScreenPrinting" className={styles.serviceSection}>
+      <Image
+        src={Banner}
+        alt="Hands pulling bright ink over a printing screen"
+      />
+      <div className={styles.sideBySide}>
+        <div className={styles.content}>
           <h2>Screen Printing</h2>
           <p>
-            {`Serving Denver and beyond since 2006, Lab Seven offers fresh takes on
-        traditional screen printing methods. We're up to speed on current
-        print-industry trends, and only use top of the line equipment and
-        materials in our Englewood, Colorado production headquarters. We're
-        constantly improving our formulas to offer bright, soft, and stunning
-        prints that you'll be as excited to share as we are to print! Stop
-        buying cheap throwaway gear, and print shirts you'll actually want to
-        wear.`}
+            {`Serving Denver and beyond since 2006, Lab Seven offers fresh takes on traditional screen printing methods. We’re up to speed on current print-industry trends, and only use top of the line equipment and materials in our Englewood, Colorado production headquarters. We're constantly improving our formulas to offer bright, soft, and stunning prints that you’ll be as excited to share as we are to print! At Lab Seven, we've perfected the science of t-shirt printing!`}
           </p>
+          <br />
+          <p>Stop buying cheap throwaway gear.</p>
           <p>
-            {`We currently offer both plastisol and water-based printing, and will be
-        re-introducing discharge screen-printing Summer of 2021.`}
+            <strong>Print shirts you'll actually want to wear.</strong>
           </p>
-          <Button>Get a Quote</Button>
-          <Button>Call Now (Outline)</Button>
-        </div>
-        <div>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              alert(`clicked ${e.targer.innerHTML}`);
+          <Button
+            className="ButtonAlternate"
+            onClick={(e) => {
+              alert(`you clicked ${e.target.innerHTML}`);
             }}
           >
-            <header>
-              <div>Image</div>
-              <h3>Get Your Free Digital Proof!</h3>
-            </header>
-            <strong>Curious about the final product?</strong>
-            <p>
-              Just upload your logo, drawing, or finished design and a Lab Seven
-              artist will be in touch with a complimentary digital proof!
-            </p>
-            <input name="__title" type="hidden" value="services" />
-            <input name="serviceName" type="hidden" value="Screen Printing" />
-            {[
-              { name: "name", label: "Name", type: "text" },
-              { name: "email", label: "Email", type: "email" },
-              {},
-            ].map(({ name, label, type }) => {
-              const id = `screenPrinting__${name}`;
-              return (
-                <div key={id}>
-                  <label htmlFor={id}>{label}</label>
-                  <input id={id} name={name} type={type} />
-                </div>
-              );
-            })}
-            <div>
-              <label htmlFor="screenPrinting__file">Attach Logo</label>
-              <input id="screenPrinting__file" name="file" type="file" />
-            </div>
-            <Button type="submit">Upload Your Logo!</Button>
-          </form>
+            Get a Quote
+          </Button>
+          <Button
+            onClick={(e) => {
+              alert(`you clicked ${e.target.innerHTML}`);
+            }}
+          >
+            Start a Project
+          </Button>
         </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            alert(`trying to submit screen printing form`);
+          }}
+          className={styles.form}
+        >
+          <header className={styles.form__header}>
+            <Image src={IconMockup} />
+            <h3>Get Your Free Digital Proof!</h3>
+          </header>
+          <strong>Curious about the final product?</strong>
+          <p>
+            Just upload your logo, drawing, or finished design and a Lab Seven
+            artist will be in touch with a complimentary digital proof!
+          </p>
+          <input name="__title" type="hidden" value="services" />
+          <input name="serviceName" type="hidden" value="Screen Printing" />
+          {[
+            { name: "name", label: "Name", type: "text" },
+            { name: "email", label: "Email", type: "email" },
+            {},
+          ].map(({ name, label, type }) => {
+            const id = `screenPrinting__${name}`;
+            return (
+              <div key={id}>
+                <label htmlFor={id}>{label}</label>
+                <input id={id} name={name} type={type} />
+              </div>
+            );
+          })}
+          <div>
+            <label htmlFor="screenPrinting__file">Attach Logo</label>
+            <input id="screenPrinting__file" name="file" type="file" />
+          </div>
+          <Button type="submit">Submit!</Button>
+        </form>
       </div>
-      <div>
-        <div>Image 2</div>
-        <div>Image 3</div>
-        <div>Image 4</div>
+      <div className={styles.imageReel}>
+        <Image src={HopeHoodies} />
+        <Image src={PhillipLindsay} />
+        <Image src={CatDog} />
       </div>
     </section>
   );
