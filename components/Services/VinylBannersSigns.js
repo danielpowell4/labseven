5e749783d98d2c4066fcdb25d90ed97643aba735
Dan Powell <dan@gobloom.io>
2023-06-08 08:53:34 -0600

    Adds vinyl banner calculator

--- components/Services/VinylBannersSigns.js
+++ components/Services/VinylBannersSigns.js
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import Image from "next/image";
 
 import { camelize } from "lib/utils";
@@ -18,19 +20,24 @@ const choices = [
   {
     label: "Material",
     options: [
-      { label: "13 oz. Standard", value: "13 oz. Standard" },
-      { label: "18 oz. Heavy Duty", value: "18 oz. Heavy Duty" },
+      { label: "13 oz. Standard", value: "13 oz. Standard", pricePerSqFt: 5 },
+      {
+        label: "18 oz. Heavy Duty",
+        value: "18 oz. Heavy Duty",
+        pricePerSqFt: 6,
+      },
       {
         label: "10 oz. Wind Resistant Mesh",
         value: "10 oz. Wind Resistant Mesh",
+        pricePerSqFt: 5.25,
       },
     ],
   },
   {
     label: "Single / Double Sided Printing",
     options: [
-      { label: "Single Sided", value: "Single Sided" },
-      { label: "Double Sided", value: "Double Sided" },
+      { label: "Single Sided", value: "Single Sided", pricePerSqFt: 0 },
+      { label: "Double Sided", value: "Double Sided", pricePerSqFt: 1.5 },
     ],
   },
   {
@@ -57,6 +64,41 @@ const VinylBannersSignsForm = () => {
     );
   }
 
+  const [values, setValues] = React.useState({});
+
+  // dimensions
+  const widthFt = values["quote.width_feet"] || 0;
+  const widthIn = values["quote.width_inches"] || 0;
+  const widthInches = widthFt * 12 + widthIn;
+  const heightFt = values["quote.height_feet"] || 0;
+  const heightIn = values["quote.height_inches"] || 0;
+  const heightInches = heightFt * 12 + heightIn;
+  const sqFt = (widthInches * heightInches) / 144;
+  const sqFtRounded = Number(sqFt.toFixed(2));
+  const setValueAsNumber = (e) => {
+    const { name, value } = e.target;
+    const numberValue = Number(value);
+    if (isNaN(numberValue)) return;
+    setValues((prev) => ({ ...prev, [name]: numberValue }));
+  };
+
+  // choices
+  const selectedMaterial = values["quote.Material"] || "";
+  const materialPricePerSqFt =
+    choices
+      .find((opt) => opt.label === "Material")
+      .options.find((opt) => opt.value === selectedMaterial)?.pricePerSqFt || 0;
+  const selectedSides = values["quote.Single / Double Sided Printing"] || "";
+  const sidePerSqFt =
+    choices
+      .find((opt) => opt.label === "Single / Double Sided Printing")
+      .options.find((opt) => opt.value === selectedSides)?.pricePerSqFt || 0;
+  const pricePerSqFt = materialPricePerSqFt + sidePerSqFt;
+  const priceShownToCustomer = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(pricePerSqFt * sqFtRounded);
+
   return (
     <form onSubmit={onSubmit} className={styles.form}>
       <header className={styles.form__header}>
@@ -68,31 +110,80 @@ const VinylBannersSignsForm = () => {
       <div className={styles.formContainerSideBySide}>
         <div className={styles.formContainer}>
           <input
-            id="banner__width"
-            name="quote.width"
+            id="banner__width_feet"
+            name="quote.width_feet"
             type="number"
-            step="0.05"
+            step="1"
+            min="0"
             className={styles.formInput}
-            placeholder="Width (in.)"
+            placeholder="Feet"
+            value={values.width_feet}
+            onChange={setValueAsNumber}
           />
-          <label htmlFor={"banner__width"} className={styles.formLabel}>
+          <label htmlFor={"banner__width_feet"} className={styles.formLabel}>
+            Width (ft.)
+          </label>
+        </div>
+        +
+        <div className={styles.formContainer}>
+          <input
+            id="banner__width_inches"
+            name="quote.width_inches"
+            type="number"
+            step="0.25"
+            min="0"
+            className={styles.formInput}
+            placeholder="Inches"
+            value={values.width_inches}
+            onChange={setValueAsNumber}
+          />
+          <label htmlFor={"banner__width_inches"} className={styles.formLabel}>
             Width (in.)
           </label>
         </div>
-        x
+      </div>
+      <div className={styles.formContainerSideBySide}>
         <div className={styles.formContainer}>
           <input
-            id="banner__height"
-            name="quote.height"
+            id="banner__height_feet"
+            name="quote.height_feet"
             type="number"
-            step="0.05"
+            step="1"
+            min="0"
             className={styles.formInput}
-            placeholder="Height (in.)"
+            placeholder="Feet"
+            value={values.height_feet}
+            onChange={setValueAsNumber}
           />
-          <label htmlFor={"banner__height"} className={styles.formLabel}>
+          <label htmlFor={"banner__height_feet"} className={styles.formLabel}>
+            Height (ft.)
+          </label>
+        </div>
+        +
+        <div className={styles.formContainer}>
+          <input
+            id="banner__height_inches"
+            name="quote.height_inches"
+            type="number"
+            step="0.25"
+            min="0"
+            className={styles.formInput}
+            placeholder="Inches"
+            value={values.height_inches}
+            onChange={setValueAsNumber}
+          />
+          <label htmlFor={"banner__height_inches"} className={styles.formLabel}>
             Height (in.)
           </label>
         </div>
+      </div>
+      <div className={styles.formContainer}>
+        <label>Dimensions</label>
+        <p style={{ margin: 0 }}>
+          {widthFt}' {widthIn}" x {heightFt}' {heightIn}" = {sqFtRounded} ft
+          <sup>2</sup>
+        </p>
+        <input type="hidden" name="quote.sq_ft" value={sqFtRounded} />
       </div>
       {choices.map(({ label, options }) => {
         const id = `banner__${camelize(label)}`;
@@ -102,12 +193,21 @@ const VinylBannersSignsForm = () => {
             <label>{label}</label>
             {options.map(({ label: optLabel, value }) => {
               const optId = `${id}__${camelize(optLabel)}`;
+              const fieldName = `quote.${label}`;
+              const formVal = values[fieldName];
               return (
                 <div key={optId}>
                   <input
                     type="radio"
                     name={`quote.${label}`}
                     value={value}
+                    checked={formVal === value}
+                    onChange={(event) =>
+                      setValues((prev) => ({
+                        ...prev,
+                        [event.target.name]: event.target.value,
+                      }))
+                    }
                     id={optId}
                   />
                   <label htmlFor={optId}>{optLabel}</label>
@@ -119,8 +219,13 @@ const VinylBannersSignsForm = () => {
       })}
       <hr />
       <div>
-        <h4>$0.00 each</h4>
+        <h4>{priceShownToCustomer} each</h4>
         <h3>Ready to Order?</h3>
+        <input
+          type="hidden"
+          name="quote.priceShownToCustomer"
+          value={priceShownToCustomer}
+        />
       </div>
       <hr />
       <div>
