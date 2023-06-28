d806b2e48ff8977092096e2befb0aa8999618578
Dan Powell <dan@gobloom.io>
2023-06-28 11:37:42 -0600

    Adds more redirects from squarespace

--- next.config.js
+++ next.config.js
@@ -9,6 +9,26 @@ module.exports = {
         destination: "/",
         permanent: false,
       },
+      {
+        source: "/threads/:slug*",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/intro",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/design",
+        destination: "/services",
+        permanent: true,
+      },
+      {
+        source: "/embroidery",
+        destination: "/services/#Embroidery",
+        permanent: true,
+      },
     ];
   },
 };
