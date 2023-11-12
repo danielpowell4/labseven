972ab12d53ac8407c624313ae645cec57aa68cdf
Dan Powell <dan@gobloom.io>
2023-11-12 11:45:34 -0700

    Adds image magnifier

--- app/gallery/[slug]/ItemGallery.js
+++ app/gallery/[slug]/ItemGallery.js
@@ -5,6 +5,94 @@ import Image from "next/image";
 
 import productStyles from "pages/product/[manufacturerSkuCode]/product.module.css";
 
+// per demo at
+function ImageMagnifier({ image }) {
+  // settings
+  const height = 400;
+  const width = 600;
+  const magnifierHeight = 200;
+  const magnifierWidth = 200;
+  const zoomLevel = 3;
+
+  const [[x, y], setXY] = React.useState([0, 0]);
+  const [[imgWidth, imgHeight], setSize] = React.useState([0, 0]);
+  const [showMagnifier, setShowMagnifier] = React.useState(false);
+  return (
+    <div
+      style={{
+        position: "relative",
+        cursor: "zoom-in",
+      }}
+    >
+      <Image
+        priority
+        src={image.url}
+        className={productStyles.gallery__main}
+        width={width}
+        height={height}
+        alt={image.alt}
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+          objectFit: "contain",
+          objectPosition: "center",
+        }}
+        onMouseEnter={(e) => {
+          // update image size and turn-on magnifier
+          const elem = e.currentTarget;
+          const { width, height } = elem.getBoundingClientRect();
+          setSize([width, height]);
+          setShowMagnifier(true);
+        }}
+        onMouseMove={(e) => {
+          // update cursor position
+          const elem = e.currentTarget;
+          const { top, left } = elem.getBoundingClientRect();
+
+          // calculate cursor position on the image
+          const x = e.pageX - left - window.pageXOffset;
+          const y = e.pageY - top - window.pageYOffset;
+          setXY([x, y]);
+        }}
+        onMouseLeave={() => {
+          // close magnifier
+          setShowMagnifier(false);
+        }}
+      />
+
+      <div
+        style={{
+          display: showMagnifier ? "" : "none",
+          position: "absolute",
+
+          // prevent magnifier blocks the mousemove event of img
+          pointerEvents: "none",
+          // set size of magnifier
+          height: `${magnifierHeight}px`,
+          width: `${magnifierWidth}px`,
+          // move element center to cursor pos
+          top: `${y - magnifierHeight / 2}px`,
+          left: `${x - magnifierWidth / 2}px`,
+          opacity: "1", // reduce opacity so you can verify position
+          border: "2px solid var(--hr)",
+          backgroundColor: "white",
+          backgroundImage: `url('${image.url}')`,
+          backgroundRepeat: "no-repeat",
+
+          // calculate zoomed image size
+          backgroundSize: `${imgWidth * zoomLevel}px ${
+            imgHeight * zoomLevel
+          }px`,
+
+          // calculate position of zoomed image.
+          backgroundPositionX: `${-x * zoomLevel + magnifierWidth / 2}px`,
+          backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+        }}
+      ></div>
+    </div>
+  );
+}
+
 function ItemGallery({ project }) {
   const images = [
     {
@@ -43,20 +131,7 @@ function ItemGallery({ project }) {
         ))}
       </ul>
 
-      <Image
-        priority
-        className={productStyles.gallery__main}
-        src={activeImage.url}
-        width={492}
-        height={738}
-        alt={activeImage.alt}
-        style={{
-          maxWidth: "100%",
-          height: "auto",
-          objectFit: "contain",
-          objectPosition: "center",
-        }}
-      />
+      <ImageMagnifier image={activeImage} />
     </div>
   );
 }
