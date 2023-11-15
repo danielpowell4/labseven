5e8a71925a07ca3be8983b43f5f0712cd168614a
Dan Powell <dan@gobloom.io>
2023-11-15 11:03:10 -0700

    Attempt 1 at og images

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -3,6 +3,7 @@
 import { Suspense } from "react";
 import InfiniteScroll from "./InfiniteScroll";
 import styles from "./gallery.module.css";
+import { sql } from "@vercel/postgres";
 
 async function GalleryPage() {
   const origin =
@@ -19,6 +20,43 @@ async function GalleryPage() {
   }
 
   return <InfiniteScroll firstPage={firstPage} />;
+}
+
+export async function generateMetadata() {
+  let images = {};
+  const { rows } =
+    await sql`SELECT projects.* FROM projects ORDER BY created_at DESC LIMIT 1`;
+  const mostRecentProject = rows[0];
+
+  if (mostRecentProject) {
+    const imgUrl = mostRecentProject?.primary_blob_url;
+    const resizedImg = `https://labseven.co/_next/image?url==${encodeURIComponent(
+      imgUrl
+    )}&w=400&q=75`;
+
+    images = {
+      "og:image": resizedImg,
+      "twitter:image": resizedImg,
+    };
+  }
+
+  return {
+    title: "Featured T-Shirt Designs Gallery | Lab Seven Screen Printing Co.",
+    description:
+      "Explore our Featured Work in the Gallery at Lab Seven Screen Printing Co. - Denver's top destination for custom t-shirt printing and design. Discover a diverse range of custom tees, showcasing our expertise in screen printing, graphic design, and embroidery. Whether you're inspired to create your own design in our studio or collaborate with our talented artists, this gallery exemplifies the creativity and quality we bring to every custom tee project in Colorado.",
+    keywords: [
+      "Lab Seven Screen Printing",
+      "Denver T-shirt Printing",
+      "Custom Tees",
+      "Screen Printing Gallery",
+      "Graphic Design",
+      "Embroidery",
+      "T-shirt Design Studio",
+      "Custom Apparel",
+      "Featured T-shirt Designs",
+    ],
+    ...images,
+  };
 }
 
 function GallerySkeleton() {
@@ -44,7 +82,6 @@ function GallerySkeleton() {
 export default async function GalleryPageWrapper() {
   return (
     <main className={styles.main}>
-      <h2>Our Work</h2>
       <Suspense fallback={<GallerySkeleton />}>
         <GalleryPage />
       </Suspense>
